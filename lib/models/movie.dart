d2361ad412ed7a61d4e35a0d4e10ea1fb335d538
aprillmezu <161106154+aprillmezu@users.noreply.github.com>
2025-03-04 12:54:49 +0700

    pilem

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,31 +1,29 @@
-import 'dart:convert';
-
 class Movie {
   final int id;
   final String title;
-  final String Overview;
+  final String overview;
   final String posterPath;
   final String backdropPath;
-  final String rilisDate;
-  final double VotaAverage;
-
+  final String releaseDate;
+  final double voteAverage;
   Movie(
-  {required this.id,
-  required this.title,
-  required this.Overview,
-  required this.posterPath,
-  required this.backdropPath,
-  required this.rilisDate,
-  required this. VotaAverage});
-
+      {required this.id,
+        required this.title,
+        required this.overview,
+        required this.posterPath,
+        required this.backdropPath,
+        required this.releaseDate,
+        required this.voteAverage});
   factory Movie.fromJson(Map<String, dynamic> json) {
-  return Movie(
+    return Movie(
       id: json['id'],
       title: json['title'],
-      Overview: json['Overview'],
-      posterPath: json['posterPath'],
-      backdropPath: json['backdropPath'],
-      rilisDate: json['rilisDate'],
-      VotaAverage: json['VotaAverage']);
+      overview: json['overview'],
+      posterPath: json['poster_path'],
+      backdropPath: json['backdrop_path'],
+      releaseDate: json['release_date'],
+      voteAverage: json['vote_average'].toDouble(),
+    );
   }
 }
+
---
+++ lib/screens/favorite_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:pilem/models/movie.dart';
+import 'package:pilem/servics/api.service.dart';
+
+class FavoriteScreen extends StatefulWidget {
+  const FavoriteScreen({super.key});
+
+  @override
+  State<FavoriteScreen> createState() => _FavoriteScreenState();
+}
+
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
